6749ebc625395202244fbd0634e9b49bab454cb9
jbatson44 <bat14009@byui.edu>
2018-05-04 16:44:49 -0600

    homepage

--- web/Homepage/homepage.php
+++ web/Homepage/homepage.php
@@ -1,8 +1,11 @@
-<html>
+<html lang="en">
 	<head>
 		<title>Jake Batson's Homepage</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
-		<p>Hello World.</p>
+		<h1>My Homepage</h2>
 	</body>
 </html>
