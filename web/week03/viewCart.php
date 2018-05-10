a87c57328089abec5f88b45638ca18ef0cef3cb3
jbatson44 <bat14009@byui.edu>
2018-05-09 20:00:31 -0600

    shop

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -15,8 +15,9 @@
 //echo "<tr><td>" . $selected . "</td><td>" .  . "</tr>";}}
 $items = $_POST["item"];
 $amounts = $_POST["number"];
+$price = $_POST["price1"];
 for ($i = 0; $i < count($items); $i++) {
-	echo "<tr><td>" . $items[$i] . "</td><td>" . $amounts[$i] . "</td></tr>";
+	echo "<tr><td>" . $items[$i] . "</td><td>" . $amounts[$i] . "</td><td>" . $price[$i] * $amounts[$i] . "</td></tr>";
 }
 ?>
 		</table>
