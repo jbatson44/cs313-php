2b1143256c33a0e78eeac83c2e8b01ea18304af3
jbatson44 <bat14009@byui.edu>
2018-05-09 19:31:16 -0600

    shop

---
+++ web/week03/viewCart.php
@@ -0,0 +1,15 @@
+<?php
+if(!empty($_POST['cart'])){
+foreach($_POST['cart'] as $selected){
+echo $selected."</br>";}}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		
+	</body>
+</html>
