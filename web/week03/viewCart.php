7e35a22552acfcffe0711b929925007d9b342deb
jbatson44 <bat14009@byui.edu>
2018-05-09 19:39:12 -0600

    shop

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -1,15 +1,20 @@
-<?php
-if(!empty($_POST['cart'])){
-foreach($_POST['cart'] as $selected){
-echo $selected."</br>";}}
-
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
 	</head>
 	<body>
-		
+		<table>
+			<tr>
+				<th>Item</th>
+				<th>Quantity</th>
+				<th>Price</th>
+			</tr>
+<?php
+if(!empty($_POST['cart'])){
+foreach($_POST['cart'] as $selected){
+echo "<tr><td>" . $selected . "</td></tr>";}}
+
+?>
+		</table>
 	</body>
 </html>
