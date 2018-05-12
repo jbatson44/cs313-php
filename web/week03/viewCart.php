efd1fd0735194b5685f2286eaf5a62f5d83cfebe
jbatson44 <bat14009@byui.edu>
2018-05-11 20:23:37 -0600

    shop

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -24,10 +24,10 @@ session_start();
 //	echo "<tr><td>" . $items[$i] . "</td><td>" . $amounts[$i] . "</td><td>" . $total . "</td></tr>";
 
 //}
-$thing1 = $_SESSION["thing1"]
-if ($thing1 > 0) {
+//$thing1 = $_SESSION["thing1"]
+//if ($thing1 > 0) {
 //	echo "<tr><td>Thing1</td><td>" . $thing1 . "</td><td>$" . $thing1 * 2 . "</td></tr>";
-}
+//}
 echo $_SESSION["thing1"];
 echo $_SESSION["thing2"];
 echo $_SESSION["thing3"];
