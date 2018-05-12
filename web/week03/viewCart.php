5d1d30be6439caf126b6b5164d710ded807c634c
jbatson44 <bat14009@byui.edu>
2018-05-12 17:57:51 -0600

    shop

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -14,26 +14,30 @@ session_start();
 			</tr>
 <?php
 if ($_SESSION['thing1'] > 0) {
+	$_SESSION['thing1price'] = $_SESSION['thing1'] * 1;
 	echo "<form method='post' action='viewCart.php'>";
-	echo "<tr><td>Thing1</td><td>" . $_SESSION['thing1'] . "</td><td>$" . $_SESSION['thing1'] * 1 . "</td>
+	echo "<tr><td>Thing1</td><td>" . $_SESSION['thing1'] . "</td><td>$" . $_SESSION['thing1price'] . "</td>
 	<td><input type='submit' name='one' value='Delete item'></td></tr>";
 	echo "</form>";
 }
 if ($_SESSION['thing2'] > 0) {
+	$_SESSION['thing2price'] = $_SESSION['thing2'] * 2;
 	echo "<form method='post' action='viewCart.php'>";
-	echo "<tr><td>Thing2</td><td>" . $_SESSION['thing2'] . "</td><td>$" . $_SESSION['thing2'] * 2 . "</td>
+	echo "<tr><td>Thing2</td><td>" . $_SESSION['thing2'] . "</td><td>$" . $_SESSION['thing2price'] . "</td>
 	<td><input type='submit' name='two' value='Delete item'></td></tr>";
 	echo "</form>";
 }
 if ($_SESSION['thing3'] > 0) {
+	$_SESSION['thing3price'] = $_SESSION['thing3'] * 3;
 	echo "<form method='post' action='viewCart.php'>";
-	echo "<tr><td>Thing3</td><td>" . $_SESSION['thing3'] . "</td><td>$" . $_SESSION['thing3'] * 3 . "</td>
+	echo "<tr><td>Thing3</td><td>" . $_SESSION['thing3'] . "</td><td>$" . $_SESSION['thing3price'] . "</td>
 	<td><input type='submit' name='three' value='Delete item'></td></tr>";
 	echo "</form>";
 }
 if ($_SESSION['thing4'] > 0) {
+	$_SESSION['thing4price'] = $_SESSION['thing4'] * 4;
 	echo "<form method='post' action='viewCart.php'>";
-	echo "<tr><td>Thing4</td><td>" . $_SESSION['thing4'] . "</td><td>$" . $_SESSION['thing4'] * 4 . "</td>
+	echo "<tr><td>Thing4</td><td>" . $_SESSION['thing4'] . "</td><td>$" . $_SESSION['thing4price'] . "</td>
 	<td><input type='submit' name='four' value='Delete item'></td></tr>";
 	echo "</form>";
 }
@@ -66,7 +70,7 @@ if (isset($_POST['four'])) {
 ?>
 		</table>
 		<?php
-		$_SESSION['total'] = $_SESSION['thing1'] + $_SESSION['thing2'] + $_SESSION['thing3'] + $_SESSION['thing4'];
+		$_SESSION['total'] = $_SESSION['thing1price'] + $_SESSION['thing2price'] + $_SESSION['thing3price'] + $_SESSION['thing4price'];
 		echo "<p><b>Total: " . $_SESSION['total'] . "</b></p>";
 		?>
 		<a href="shop.php">Return to browse page</a>
