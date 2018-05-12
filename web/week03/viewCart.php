97107a40f299eb07734c93a414515dd216644ed5
jbatson44 <bat14009@byui.edu>
2018-05-12 17:36:40 -0600

    shop

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -45,17 +45,17 @@ if (isset($_POST['one'])) {
 	unset($_POST['one']);
 }
 if (isset($_POST['two'])) {
-	$_SESSION['thing1'] = 0;
+	$_SESSION['thing2'] = 0;
 	echo "<meta http-equiv='refresh' content='0'>";
 	unset($_POST['two']);
 }
 if (isset($_POST['three'])) {
-	$_SESSION['thing1'] = 0;
+	$_SESSION['thing3'] = 0;
 	echo "<meta http-equiv='refresh' content='0'>";
 	unset($_POST['three']);
 }
 if (isset($_POST['four'])) {
-	$_SESSION['thing1'] = 0;
+	$_SESSION['thing4'] = 0;
 	echo "<meta http-equiv='refresh' content='0'>";
 	unset($_POST['four']);
 }
