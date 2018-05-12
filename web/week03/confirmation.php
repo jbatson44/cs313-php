5489c19e16817f00dc4d9d1a3fc97dfe16a8545f
jbatson44 <bat14009@byui.edu>
2018-05-12 17:47:29 -0600

    shop

--- web/week03/confirmation.php
+++ web/week03/confirmation.php
@@ -9,8 +9,25 @@ session_start();
 	</head>
 	<body>
 	<h1>Confirmation</h1>
+	<table>
+		<tr>
+			<th>Item</th>
+			<th>Quantity</th>
+			<th>Price</th>
+		</tr>
 <?php
-echo "Thing1 =" . $_SESSION['thing1'];
+if ($_SESSION['thing1'] > 0) {
+	echo "<tr><td>Thing1</td><td>" . $_SESSION['thing1'] . "</td><td>$" . $_SESSION['thing1'] * 1 . "</td></tr>"
+}
+if ($_SESSION['thing2'] > 0) {
+	echo "<tr><td>Thing2</td><td>" . $_SESSION['thing2'] . "</td><td>$" . $_SESSION['thing2'] * 2 . "</td></tr>"
+}
+if ($_SESSION['thing3'] > 0) {
+	echo "<tr><td>Thing3</td><td>" . $_SESSION['thing3'] . "</td><td>$" . $_SESSION['thing3'] * 3 . "</td></tr>"
+}
+if ($_SESSION['thing4'] > 0) {
+	echo "<tr><td>Thing4</td><td>" . $_SESSION['thing4'] . "</td><td>$" . $_SESSION['thing4'] * 4 . "</td></tr>"
+}
 ?>
 
 	</body>
