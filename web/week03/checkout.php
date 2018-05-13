e21f66bd82e8f3e44e80ee1c44c4ef764ea59700
jbatson44 <bat14009@byui.edu>
2018-05-12 19:42:25 -0600

    shop

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -13,27 +13,19 @@ session_start();
 		<form method="post" onsubmit="return required()" action="confirmation.php">
 			<div id="cityName">
 				City: <br>
-				<input oninput="validateCity()" type="text" name="city"><span style="color:red"> * </span>
-				<span class="city" style="color:red">Don't leave blank</span>
+				<input oninput="validateCity()" type="text" name="city">
 			</div>
 			<div id="stateid">
 				State: <br>
 				<input oninput="validateState()" type="text" name="state" maxlength="2">
-				<span style="color:red">* </span>
-				<span class="sta" style="color:red">Not a state</span><br>
 			</div>
 			<div id="billAddress">
 				Billing address: <br>
 				<input oninput="validateAddress()" type="text" name="address">
-				<span style="color:red">* </span>
-				<span class="address" style="color:red">Don't leave blank</span>
 			</div>
 			<div id="zipCode">
 				Zip or postal code: <br>
-				<input oninput="validateZip()" 
-                   type="text" name="zip">
-				<span style="color:red">* </span>
-				<span class="zip" style="color:red">Invalid</span>
+				<input oninput="validateZip()" type="text" name="zip">
 			</div><br>
 			<input type='submit' name='submit' value='Complete purchase'>
 		</form>
