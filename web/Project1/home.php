187701033861e068c457f921390e57e5574067dd
jbatson44 <bat14009@byui.edu>
2018-05-23 17:04:40 -0600

    testing output

--- web/Project1/home.php
+++ web/Project1/home.php
@@ -12,8 +12,26 @@ $dbName = ltrim($dbopts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-echo $dbUser;
 ?>
 
-it works!
+
+<table>
+	<tr>
+		<th>Exercises</th>
+	</tr>
+<?php
+
+$statement = $db->prepare("SELECT exercise FROM exercises");
+$statement->execute();
+// Go through each result
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	echo "<tr><td>" . $row['exercise'] . "</td></tr>";
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	
+}
+?>
+</table>
