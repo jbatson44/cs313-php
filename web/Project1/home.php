ed51333c62235fa0a1ec4f38a07cbff88ca0d013
jbatson44 <bat14009@byui.edu>
2018-05-24 17:41:26 -0600

    make users links

--- web/Project1/home.php
+++ web/Project1/home.php
@@ -28,7 +28,7 @@ $statement->execute();
 echo "<ul>";
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-	echo "<li>" . $row['username'] . "</li>";
+	echo "<li><a href='$row['username'].php'>" . $row['username'] . "</a></li>";
 }
 echo "</ul>";
 /*
