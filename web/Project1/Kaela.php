b67fdd0a597e04f291c5dea64249db2d3b22540f
jbatson44 <bat14009@byui.edu>
2018-05-24 18:14:30 -0600

    made user pages

--- web/Project1/Kaela.php
+++ web/Project1/Kaela.php
@@ -12,20 +12,26 @@ $dbName = ltrim($dbopts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $db->prepare("SELECT * FROM users WHERE username='Kaela'");
+$statement->execute();
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>User Kaela</title>
+<?php
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	echo "<title>User " . $row['username'] . "</title>";
+}
+?>
 	</head>
 	<body>
 <?php
-$statement = $db->prepare("SELECT * FROM users WHERE username='Kaela'");
-$statement->execute();
 // Go through each result
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-	echo $row['username'];
+	echo "<h1>" . $row['username'] . "</h1>";
 }
 ?>
 	</body>
