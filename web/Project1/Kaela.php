3c8510034c343a6778311e5a9d617e471341323f
jbatson44 <bat14009@byui.edu>
2018-05-24 18:07:41 -0600

    made user pages

--- web/Project1/Kaela.php
+++ web/Project1/Kaela.php
@@ -1,3 +1,18 @@
+<?php
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,7 +20,7 @@
 	</head>
 	<body>
 <?php
-$statement = $db->prepare("SELECT * FROM users");
+$statement = $db->prepare("SELECT * FROM users WHERE username='Kaela'");
 $statement->execute();
 // Go through each result
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
