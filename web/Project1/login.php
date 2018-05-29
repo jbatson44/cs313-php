136f94a1deca3b8b80f62cf24e340ce1acb04cdf
jbatson44 <bat14009@byui.edu>
2018-05-29 14:36:40 -0600

    added create an account page

--- web/Project1/login.php
+++ web/Project1/login.php
@@ -24,7 +24,9 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			Username: 
 			<input type="text" name="username"><br><br>
 			Password: 
-			<input type="text" name="password"><br>
+			<input type="text" name="password"><br><br>
+			<input type="submit" value="login"><br>
+			<a href="createAccount.php">Create an account</a>
 		</div>
 	</body>
 </html>
