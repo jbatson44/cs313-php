2b97fa6d894575975c24123cfafa5a095aec866a
jbatson44 <bat14009@byui.edu>
2018-05-29 15:10:19 -0600

    check password and send to user page

--- web/Project1/testLogin.php
+++ web/Project1/testLogin.php
@@ -24,10 +24,22 @@ $user = $_POST['username'];
 $pass = $_POST['password'];
 $statement = $db->prepare("SELECT * FROM users");
 $statement->execute();
-
+$userRight = false;
+$passRight = false;
 while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    if ($row['username'] == $user)
-		echo "Welcome " . $user;
+    if ($row['username'] == $user) {
+		$userRight = true;
+		if (row['password'] == $pass) {
+			$passRight = true;
+			header("location: https://glacial-meadow-56606.herokuapp.com/Project1/" . $user . ".php");  
+			exit(); 
+		}
+	}
+}
+if ($userRight == false) {
+	echo "Username doesn't exist!";
+	echo "<a href='createAccount.php'>Create an new account</a>";
+	echo "<a href='login.php'>Retry login</a>";
 }
 ?>
 		
