e0cf696db2fa33709dba3933a8b8acac9aaf1845
jbatson44 <bat14009@byui.edu>
2018-05-31 15:34:25 -0600

    insert new account into database and links to routines.php

--- web/Project1/testAccount.php
+++ web/Project1/testAccount.php
@@ -40,7 +40,28 @@ if ($userExists == true) {
 	echo "<a href='login.php'>Login as existing user</a><br>";
 }
 else {
-	
+	try
+	{
+		$query = 'INSERT INTO users(username, password, heightfeet, heightinch, weight) VALUES(:username, :password, :heightfeet, :heightinch, :weight)';
+		$statement = $db->prepare($query);
+		// Now we bind the values to the placeholders. This does some nice things
+		// including sanitizing the input with regard to sql commands.
+		$statement->bindValue(':username', $user);
+		$statement->bindValue(':password', $pass);
+		$statement->bindValue(':heightfeet', $heightfeet);
+		$statement->bindValue(':heightinch', $heightinch);
+		$statement->bindValue(':weight', $weight);
+		$statement->execute();
+		header("location: https://glacial-meadow-56606.herokuapp.com/Project1/routines.php");  
+		exit(); 
+	}
+	catch (Exception $ex)
+	{
+		// Please be aware that you don't want to output the Exception message in
+		// a production environment
+		echo "Error with DB. Details: $ex";
+		die();
+	}
 }
 ?>
 		
