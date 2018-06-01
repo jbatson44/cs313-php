343dd4b64f6659c58616485eb28e0810bacb9c18
jbatson44 <bat14009@byui.edu>
2018-05-31 21:34:42 -0600

    make routines.php unique for each user

--- web/Project1/testLogin.php
+++ web/Project1/testLogin.php
@@ -22,7 +22,8 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	</head>
 	<body>
 <?php
-$_SESSION['user'] = $_POST['username'];
+$user = $_POST['username'];
+$_SESSION['user'] = $user;
 $pass = $_POST['password'];
 $statement = $db->prepare("SELECT * FROM users");
 $statement->execute();
