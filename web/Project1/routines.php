e3a3f4eb7fce14838a367f415c6a5093e3a19a6b
jbatson44 <bat14009@byui.edu>
2018-05-31 22:10:54 -0600

    make routines.php unique for each user

--- web/Project1/routines.php
+++ web/Project1/routines.php
@@ -52,8 +52,8 @@ $routineid = array_unique($routid);
 $routReal = array_combine($routineid, $routine);
 
 
-	echo $_SESSION['user'];
-	echo "<h1>" . $username . "</h1>";	
+	//echo $_SESSION['user'];
+	echo "<h1>" . $_SESSION['user'] . "</h1>";	
 	echo "Height: " . $heightfeet . "'" . $heightinch . "\"<br>";
 	echo "Current weight: " . $weight . " lbs<br>";
 	echo "Userid: " . $userid;
