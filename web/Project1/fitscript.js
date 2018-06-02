8a7a2f582d1c90017c5500e7937eed42d6415146
jbatson44 <bat14009@byui.edu>
2018-06-02 13:57:05 -0600

    allow editing of routines

--- web/Project1/fitscript.js
+++ web/Project1/fitscript.js
@@ -3,7 +3,7 @@ function editRoutines() {
 		console.log("show!");
 		document.getElementsByName("newRoutine")[0].style.visibility = 'visible';
 		document.getElementsByName("submitRoutine")[0].style.visibility = 'visible';
-	} else //if(document.getElementsByName("newRoutine")[0].style.visibility == 'visible') {
+	} else {//if(document.getElementsByName("newRoutine")[0].style.visibility == 'visible') {
 		console.log("show!");
 		document.getElementsByName("newRoutine")[0].style.visibility = 'hidden';
 		document.getElementsByName("submitRoutine")[0].style.visibility = 'hidden';
