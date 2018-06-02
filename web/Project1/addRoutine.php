ff5d677ec0ac6be6a485d059deb6f1c016aa1118
jbatson44 <bat14009@byui.edu>
2018-06-02 14:08:14 -0600

    allow editing of routines

--- web/Project1/addRoutine.php
+++ web/Project1/addRoutine.php
@@ -20,10 +20,33 @@ try {
 	
 	$statement->bindValue(':routine', $newRoutine);
 	$statement->bindValue(':userid', $_SESSION['userid']);
+	$statement->execute();
 	header("location: https://glacial-meadow-56606.herokuapp.com/Project1/routines.php");  
 	exit();
 } catch(Exception $ex) {
+	echo "Error with DB. Details: $ex";
+	die();
+}
+/*
+try
+	{
+		$query = 'INSERT INTO routines(routine, userid) VALUES(:routine, :userid)';
+		$statement = $db->prepare($query);
+	
+		$statement->bindValue(':username', $user);
+		$statement->bindValue(':password', $pass);
+		$statement->bindValue(':heightfeet', $heightfeet);
+		$statement->bindValue(':heightinch', $heightinch);
+		$statement->bindValue(':weight', $weight);
+		$statement->execute();
+		header("location: https://glacial-meadow-56606.herokuapp.com/Project1/routines.php");  
+		exit(); 
+	}
+	catch (Exception $ex)
+	{
+		// Please be aware that you don't want to output the Exception message in
+		// a production environment
 		echo "Error with DB. Details: $ex";
 		die();
-}
+	}*/
 ?>
