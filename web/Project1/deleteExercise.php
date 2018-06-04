85b7f1661bdc18dd22c79709be7b07df6d1e738b
jbatson44 <bat14009@byui.edu>
2018-06-04 16:25:04 -0600

    add ability to delete exercies

---
+++ web/Project1/deleteExercise.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$delExercise = $_POST['deleteExid'];
+
+try {
+$query = "DELETE FROM exercise WHERE exerciseid = :id";
+	$statement = $db->prepare($query);
+	
+	$statement->bindValue(':id', $delExercise);
+	//$statement->bindValue(':userid', $_SESSION['userid']);
+	$statement->execute();
+	header("location: https://glacial-meadow-56606.herokuapp.com/Project1/routines.php");  
+	exit();
+} catch(Exception $ex) {
+	echo "Error with DB. Details: $ex";
+	die();
+}
+?>
