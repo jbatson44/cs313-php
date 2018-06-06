b90ae6fdec1d27abb4d54c475098c578b75e0905
jbatson44 <bat14009@byui.edu>
2018-06-05 20:05:29 -0600

    TA07 sign up page

--- web/ta07/welcome.php
+++ web/ta07/welcome.php
@@ -1,10 +1,10 @@
 <?
 session_start();
-//if(isset($_SESSION['user'])) {
-	echo "Welcome" . $_SESSION['user'];
-//} else {
-//	header("location: https://glacial-meadow-56606.herokuapp.com/ta07/signin.php");  
-//}
+if(isset($_SESSION['user'])) {
+	echo "Welcome " . $_SESSION['user'];
+} else {
+	header("location: https://glacial-meadow-56606.herokuapp.com/ta07/signin.php");  
+}
 
 ?>
 
