fa22c0c0e5591fb9145f967e89763b2a9bc5a39e
jbatson44 <bat14009@byui.edu>
2018-06-09 10:13:21 -0600

    improve layout

--- web/Project1/routines.php
+++ web/Project1/routines.php
@@ -13,8 +13,6 @@ $dbName = ltrim($dbopts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,31 +31,21 @@ $statement->execute();
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	$_SESSION['userid'] = $row['id'];
-	//echo $row['userid'];
-	//$username = $row['username'];
 	$_SESSION['heightfeet'] = $row['heightfeet'];
 	$_SESSION['heightinch'] = $row['heightinch'];
 	$_SESSION['weight'] = $row['weight'];
 }
-//$statement = $db->prepare("SELECT * FROM users FULL OUTER JOIN routines ON routines.userid=userid WHERE userid = '5'");
-//$statement->execute();
+
 $statement = $db->prepare("SELECT * FROM routines WHERE userid = '{$_SESSION['userid']}'");
 $statement->execute();
 $routine = array();
 $routineid = array();
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-	//$userid = $row['userid'];
-	//$username = $row['username'];
-	//$heightfeet = $row['heightfeet'];
-	//$heightinch = $row['heightinch'];
-	//$weight = $row['weight'];
-	
 	$routid[] = $row['routineid'];
-	
 	$rout[] = $row['routine']; 
 }
-//echo "<title>User " . $username . "</title>";
+
 $routine = array_unique($rout);
 $routineid = array_unique($routid);
 $routReal = array_combine($routineid, $routine);
@@ -72,54 +60,28 @@ $routReal = array_combine($routineid, $routine);
 			<h2>Routines</h2>
 		
 <?php
-//$in = join(',', array_fill(0, count($routineid), '?'));
-//$select = <<<SQL
-   // SELECT *
-   // FROM exercises
- //   WHERE id IN ($in);
-//SQL;
-//$statement = $pdo->prepare($select);
-//$statement->execute($routineid);
+
 $statement = $db->prepare("SELECT * FROM exercises");
 $statement->execute();
 $exercises = array();
 $exRoutid = array();
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-	//if (row['routineid'] == '2')
-		$exercises[] = $row['exercise'];
-		$exRoutid[] = $row['routineid'];
-		$exid[] = $row['routineid'];
-}
-//$routine = array_unique($rout);
-//$routineid = array_unique($routid);
-$exReal = array_combine($exercises, $exRoutid);
-//while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-//{
-//foreach ($routine as $value) {
-//    echo "<h3>" . $value . "</h3>";
-//}
 
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+	$exercises[] = $row['exercise'];
+	$exRoutid[] = $row['routineid'];
+	$exid[] = $row['routineid'];
+}
+
+$exReal = array_combine($exercises, $exRoutid);
 ?>
 <input type="button" name="edit" value="+" onclick="addRoutines()"/>
 <input type="button" name="edit" value="-" onclick="deleteRoutines()"/>
 <?php
-/*
-foreach ($routReal as $key => $value) {
-	echo "<h3>" . $value . "</h3>";
-	echo "<ul>";
-	foreach ($exReal as $exkey => $exvalue) {
-		if ($key == $exvalue)
-			echo "<li>" . $exkey . "</li>";
-	}
-	echo "</ul>";
-}
-*/
 $newArray = array();
 for ($i = 0; $i < count($exercises); $i++) {
 	$newArray[$i] = array($exercises[$i], $exid[$i], $exRoutid[$i]);
 }
-
+echo "<div class='middle'>";
 foreach ($routReal as $key => $value) {
 	echo "<div class='routine'><h3>" . $value . "</h3>";
 	//echo "<ul>";
@@ -146,25 +108,9 @@ session_write_close();
 	<input type="text" class="edits" name="newRoutine" style="visibility:hidden">
 	<input type="submit" class="edits" value="Add Routine" name="submitRoutine" style="visibility:hidden">
 </form>
+</div>
 <?php
-//foreach ($routineid as $value) {
-//	echo $value;
-//}
-//foreach ($routReal as $key => $value) {
-//	echo "<h3>" . $key . " " . $value . "</h3>";
-//}
-//}
-/*$statement = $db->prepare("SELECT * FROM exercises WHERE routineid='1'");
-$statement->execute();
-echo "<ul class='routine'>";
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	$exerciseid = $row['exerciseid'];
-	$exercise = $row['exercise'];
-	echo "<li>" . $exercise . "</li>"; 
-}
-echo "</ul>";
-*/
+
 ?>
 	</body>
 </html>
